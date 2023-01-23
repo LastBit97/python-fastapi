ef4afe30b0fda86d8b335b3259aa23c9824422e5
LastBit97 <diman01.97@gmail.com>
2023-01-23 15:43:12 +0300

    implement filtering logic

--- api/router.py
+++ api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from api.service import case_sensitive_filtering
 
 router = APIRouter()
 
 
-@router.get("/")
-async def root():
-    return {"message": "Hello World"}
+@router.post("/filter/case_sensitive")
+async def filter_case_sensitive(data: list):
+    return await case_sensitive_filtering.filter_words(data)
