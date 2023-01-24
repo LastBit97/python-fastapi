7c96144cafa28a28d3b5eec6b3569b6542b67267
LastBit97 <diman01.97@gmail.com>
2023-01-24 13:10:41 +0300

    implement combining files service

--- api/router.py
+++ api/router.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, HTTPException
 from api.service import case_sensitive_filtering
+from api.service import combining_data
+import pandas as pd
+from io import BytesIO
 
 router = APIRouter()
 
@@ -7,3 +10,23 @@ router = APIRouter()
 @router.post("/filter/case_sensitive")
 async def filter_case_sensitive(data: list):
     return await case_sensitive_filtering.filter_words(data)
+
+
+@router.post("/upload")
+async def combine_data_files(files: list[UploadFile]):
+    error_files = []
+    for file in files:
+        if not file.filename.endswith((".csv", ".json")):
+            error_files.append(file.filename)
+    if error_files:
+        raise HTTPException(status_code=415, detail=error_files)
+    await combining_data.combine_files(files)
+    return {"filenames": [file.filename for file in files]}
+
+
+        # contents = await file.read()
+        # data = BytesIO(contents)
+        # df = pd.read_csv(data)
+        # print(df)
+        # data.close()
+
--- api/service/combining_data.py
+++ api/service/combining_data.py
@@ -1,21 +1,24 @@
+from io import BytesIO, StringIO
 import pandas as pd
-from glob import glob
-import os
+from fastapi import UploadFile
 
-li = []
-all_files_csv = glob('*.csv')
 
-for filename in all_files_csv:
-    df = pd.read_csv(filename, sep=';', index_col=None, header=0)
-    li.append(df)
-
-all_files_json = glob('*.json')
-
-for filename in all_files_json:
-    df = pd.read_json(filename)
-    li.append(df)
-
-dataframe = pd.concat(li, axis=0, ignore_index=True)
-
-print(dataframe)
-# dataframe.to_csv("file_name.csv", sep=';')
+async def combine_files(files: list[UploadFile]):
+    li = []
+    for file in files:
+        if file.filename.endswith(".csv"):
+            content = await file.read()
+            data = BytesIO(content)
+            df = pd.read_csv(data, sep=';')
+            li.append(df)
+            data.close()
+        elif file.filename.endswith(".json"):
+            content = await file.read()
+            s = str(content, 'utf-8')
+            data = StringIO(s)
+            df = pd.read_json(data)
+            li.append(df)
+            data.close()
+    dataframe = pd.concat(li, axis=0, ignore_index=True)
+    print(dataframe)
+    dataframe.to_csv("C:/Users/ostbi/pytnon/python-fastapi/data/file_name.csv", sep=';')
